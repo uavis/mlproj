c23fbaef26e1268f760cee5f4955347d7c7e711e
sepehr <valipour@ualberta.ca>
2015-11-06 16:00:08 -0700

    .

--- BuildingsRoads/classification.m
+++ BuildingsRoads/classification.m
@@ -1,8 +1,8 @@
 function classification (labels_train, X_train, labels_test, X_test, params)
-    if strcmp (params.clssifier,'logistic_reg')
+    if strcmp (params.classifier,'logistic_reg')
         B = mnrfit(X_train,labels_train);
         pihat = mnrval(B,X_test);
-    elseif strcmp (params.clssifier,'svm')
+    elseif strcmp (params.classifier,'svm')
         model = libsvmtrain( labels_train, X_train);
         [predict_label, accuracy] = libsvmpredict(labels_test, X_test, model);
     end
