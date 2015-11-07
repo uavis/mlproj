f64f8843da1050d5717c807e6fa9ba5385b17801
mennatulSiam <mennatul@ualberta.ca>
2015-11-06 20:11:22 -0700

    Modified Whitening to work with both Grayscale and RGB

--- BuildingsRoads/zcawhitening.m
+++ BuildingsRoads/zcawhitening.m
@@ -1,28 +1,15 @@
 function imgZCAwhite= zcawhitening(patches, params)
     disp('ZCA Whitening...');
-
-    imgZCAwhite= zeros(size(patches, 1), params.rfSize(1)* params.rfSize(2));
-    for i=1: size(patches, 1)
-        img= patches(i, :);
-        %img= rgb2gray(img);
-        img= im2double(img);
-        img= reshape( img, params.rfSize(1), params.rfSize(2));
-        %already contrast normalization is done in extract_patches function
-        %avg = mean(img, 1);
-        %img = img - repmat(avg, size(img, 1), 1);
+    img= im2double(patches);
     
-        cov = img * img' / size(img, 2);
-        [U,S,V] = svd(cov);
+    cov = img' * img / size(img, 2);
+    [U,S,V] = svd(cov);
 
-        %imgPCAwhite = diag(1./sqrt(diag(S) + eps)) * U' * img;
-        imgZCA = U * diag(1./sqrt(diag(S) + 0.1 )) * U' * img;
-        imgZCAwhite(i, :)= reshape(imgZCA,1,size(imgZCA,1)*size(imgZCA,2));
-        %figure(1), imshow(img);
-        %figure(2), imshow(imgZCA);
-        %pause
-        
-        %cov2 = imgZCAwhite * imgZCAwhite' / size(imgZCAwhite, 2);
-        %error= max(sum(abs(cov2-eye(size(imgZCAwhite, 1), size(imgZCAwhite, 2)))))
-    end
+    imgZCAwhite = (U * diag(1./sqrt(diag(S) + 0.1 )) * U' * img')';
     
+%     img= reshape( patches(1, :), params.rfSize(1), params.rfSize(2));
+%     figure(1), imshow(img);
+%     imgZ= reshape( imgZCAwhite(1, :), params.rfSize(1), params.rfSize(2));
+%     figure(2), imshow(imgZ);
+%     pause;
 end
