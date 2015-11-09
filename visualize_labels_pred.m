4d8afdd2feb81f7918d9f801f9c5760b33de06f5
Sepideh Hosseinzadeh <sepideh@richdale.(none)>
2015-11-09 14:44:21 -0700

    chenging the code visualize_labels_pred

--- visualize_labels_pred.m
+++ visualize_labels_pred.m
@@ -4,27 +4,24 @@ I = V(:,:,slice_index);
 imshow(I,[])
 hold on
 
+%% visualize labels for groundtruth
 BW = gt(:,:,slice_index);
 B = bwboundaries(BW); % extract the contour, use it as ground truth
+for i=1:length(B)
+    ground_truth = B{i};
+    ground_truth = ground_truth(:,[2 1]); % swap the columns
+    plot(ground_truth(:,1), ground_truth(:,2), 'g');
+end
 
-ground_truth = B{1};
-ground_truth = ground_truth(:,[2 1]); % swap the columns
-plot(ground_truth(:,1), ground_truth(:,2), 'g');
-
-ground_truth = B{2};
-ground_truth = ground_truth(:,[2 1]); % swap the columns
-plot(ground_truth(:,1), ground_truth(:,2), 'g');
-
+%% visualize labels for prediction
 BW = preds>0.5;
 B = bwboundaries(BW); % extract the contour, use it as ground truth
+for i=1:length(B)
+    ground_truth = B{i};
+    ground_truth = ground_truth(:,[2 1]); % swap the columns
+    plot(ground_truth(:,1), ground_truth(:,2), 'r');
+end
 
-ground_truth = B{1};
-ground_truth = ground_truth(:,[2 1]); % swap the columns
-plot(ground_truth(:,1), ground_truth(:,2), 'r');
-
-ground_truth = B{2};
-ground_truth = ground_truth(:,[2 1]); % swap the columns
-plot(ground_truth(:,1), ground_truth(:,2), 'r');
 
 hold off
 
