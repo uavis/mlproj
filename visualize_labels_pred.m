09b4af2cae2f68c742511bb65f7a5dad1a52b99d
vincentzhang <vincent.zhang@ualberta.ca>
2015-11-10 22:43:39 -0700

    Clean up the visualization function and fix an issue with the legend

--- visualize_labels_pred.m
+++ visualize_labels_pred.m
@@ -1,4 +1,10 @@
 function visualize_labels_pred(V, gt, preds, vol_index, slice_index)
+% Overlays the annotations and predictions onto the original scan.
+% Input:
+%       V: a 3D volume
+%       gt: ground truth annotations
+%       preds: predictions
+%       vol_index, slice_index: index of the volume and the slice(z-index)
 
 I = V(:,:,slice_index);
 imshow(I,[])
@@ -9,20 +15,22 @@ BW = gt(:,:,slice_index);
 B = bwboundaries(BW); % extract the contour, use it as ground truth
 for i=1:length(B)
     ground_truth = B{i};
-    ground_truth = ground_truth(:,[2 1]); % swap the columns
-    plot(ground_truth(:,1), ground_truth(:,2), 'g');
+    ground_truth = ground_truth(:,[2 1]); % swap the column
+    % It's okay to overwrite the handle since we only need one for plotting the legends
+    h1 = plot(ground_truth(:,1), ground_truth(:,2), 'g');
 end
 
 %% visualize labels for prediction
 BW = preds>0.5;
-B = bwboundaries(BW); % extract the contour, use it as ground truth
+B = bwboundaries(BW); % extract the contour of the prediction
 for i=1:length(B)
     ground_truth = B{i};
     ground_truth = ground_truth(:,[2 1]); % swap the columns
-    plot(ground_truth(:,1), ground_truth(:,2), 'r');
+    % It's okay to overwrite the handle since we only need one for plotting the legends
+    h2 = plot(ground_truth(:,1), ground_truth(:,2), 'r');
 end
 
-legend([h1 h3],{'Ground Truth','Prediction'});
+legend([h1 h2],{'Ground Truth','Prediction'});
 title(sprintf('Segmentation Result on volume %d, slice %d',vol_index, slice_index));
 hold off
 
