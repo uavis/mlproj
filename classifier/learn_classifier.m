037f8bd572a6384ec5b6fadf8c1fd28a2af0e3bb
Mandytang <cntangmin@gmail.com>
2015-11-12 18:42:51 -0700

    add cv accuracy

--- classifier/learn_classifier.m
+++ classifier/learn_classifier.m
@@ -2,7 +2,7 @@ function [model, scaleparams] = learn_classifier(X, labels, numfolds)
 
     % Values to search over
     % This is going to be used as the regularization rate
-    vals = 2.^[-5:15];
+    vals = 2.^[-10:10];
     
     % First permute the data
     indperm = randperm(size(X, 1));
@@ -19,4 +19,9 @@ function [model, scaleparams] = learn_classifier(X, labels, numfolds)
     [X, scaleparams] = standard(X);
     scaleparams.optval = optval;
     model = softmax_regression(X, labels, 2, optval);
+    
+    % cross validation accuracy
+    disp('Computing cross validation accuracy...')
+    [cvAcc] = accuracy_CV(X, labels, vals, numfolds)
+    disp(['Cross Validation Accuracy: ' num2str(max(cvAcc) * 100) '%']);
 
